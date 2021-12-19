f543e5b9dfde44e12ba14f52904cfd5b698722c8
lismarifn <lismarifn@gmail.com>
2021-12-18 22:54:06 -0500

    Usuario y Token Listo

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-//requires
+// Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-
-//inicializar var
+// Inicializar variables
 var app = express();
 
-//conect db
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+// Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
 
     if (err) throw err;
 
-    console.log('Data Base online');
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 });
 
-//rutas
-app.get('/', (req, resp, next) => {
-    resp.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    })
 
-});
+// Rutas
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+app.use('/usuario', usuarioRoutes);
 
-//escuchar peticiones
+
+// Escuchar peticiones
 app.listen(8090, () => {
-    console.log('Express serve port 8090');
+    console.log('Express server puerto 8090: \x1b[32m%s\x1b[0m', 'online');
 });
